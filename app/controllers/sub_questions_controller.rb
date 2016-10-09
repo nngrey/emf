1ce6030c16a81bf53f10cb82f7d7f002a05e0be0
nngrey <greynng@gmail.com>
2016-10-09 12:27:43 -0700

    clean up views

--- app/controllers/sub_questions_controller.rb
+++ app/controllers/sub_questions_controller.rb
@@ -1,14 +1,3 @@
 class SubQuestionsController < ApplicationController
-  # def edit
-  #   binding.pry
-  # end
 
-  # private
-  # def sub_question_params
-  #   params.require(:sub_question).permit(
-  #     :monitoring_information,
-  #     :description,
-  #     performance_indicators_attributes: [:description, :definition, :numerator, :denominator :_destroy]
-  #     )
-  # end
 end
--- app/models/evaluative_question.rb
+++ app/models/evaluative_question.rb
@@ -2,4 +2,7 @@ class EvaluativeQuestion < ApplicationRecord
   has_many :sub_questions, inverse_of: :evaluative_question
 
   accepts_nested_attributes_for :sub_questions, allow_destroy: true
+
+  validates :category, presence: true
+  validates :description, presence: true
 end
